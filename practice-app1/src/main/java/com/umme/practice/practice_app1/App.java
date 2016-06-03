f91e6dd000c174447e26fbca8ea3971c7ed5daa9
ummehunn <ummehunn@ca.ibm.com>
2016-06-03 16:25:21 -0400

    Initial commit for practice app

---
+++ practice-app1/src/main/java/com/umme/practice/practice_app1/App.java
@@ -0,0 +1,13 @@
+package com.umme.practice.practice_app1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
